2e952d4fc97f640d881f515a0cdcc51059473a51
melumilic <56738739+melumilic@users.noreply.github.com>
2021-06-12 23:21:34 -0700

    updated nextLevel Script

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameManager gameManager;
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if (gameManager == null)
+        {
+            Debug.Log("GameManager in spike doesnt exist please drag a GameManager into the scene");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player"){
+            gameManager.RestartLevel();
+        }
+    }
+}
